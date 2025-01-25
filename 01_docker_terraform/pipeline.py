32523a521769d90409b77029321cbb599e4ffb32
Badr Bensassi <you@example.com>
2025-01-26 00:54:17 +0100

    first commit

---
+++ 01_docker_terraform/pipeline.py
@@ -0,0 +1,11 @@
+import sys 
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fancy stuff here
+
+print("job finished for day = {day}")
---
+++ README.md
@@ -0,0 +1,3 @@
+Data Engineering ZoomCamp - Cohort 2025
+
+My personal repo for the 2025 cohort where I push my code for later use!
